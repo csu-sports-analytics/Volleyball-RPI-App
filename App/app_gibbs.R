99eb4adac1a58cab2d196bff332a7368627c02ce
Oliver Shanklin <oliver.shanklin@outlook.com>
2019-08-20 16:15:14 -0600

    Got a working calculation

--- App/app_gibbs.R
+++ App/app_gibbs.R
@@ -26,79 +26,6 @@ full_schedules <- read.csv("./data/full_schedules.csv", header = T, stringsAsFac
 #-------------------------------------------------------------------------------
 # DEFINE HELPER FUNCTIONS
 #-------------------------------------------------------------------------------
-recalculate_rpi <- function(start_team_id, game_to_replace, date_of_game_to_replace, new_game, result){
-  
-  # get date object to replace
-  date_of_game_to_replace_format <- mdy(date_of_game_to_replace)
-  
-  replace_org <- filter(master_team_list, team == game_to_replace)
-  
-  new_row <- c(as.integer(start_team_id), as.integer(replace_org$team_id), date_of_game_to_replace, date_of_game_to_replace_format, game_to_replace, NA, result, NA, NA)
-  
-  # # get selected teams full schedule without game to remove
-  # schedule_minus_game <- full_schedules %>%
-  #   filter(., team_id == start_team_id,
-  #          !(opponent == game_to_replace & date == date_of_game_to_replace))
-  # 
-  # 
-  # 
-  # opponents <- schedule_minus_game$org_id
-  # 
-  # opp_schedules <- opp_schedules <- full_schedules %>%
-  #   filter(., team_id %in% opponents,
-  #          !(org_id == start_team_id))
-  # 
-  # opp_wl <- (opp_schedules$result == "W")
-  # mean(opp_wl)
-  # 
-  ###
-  
-  list_opp <- full_schedules %>%
-    filter(., team_id == start_team_id,
-           !(opponent == game_to_replace & date == date_of_game_to_replace)) %>%
-    rbind(., new_row) %>%
-    select(., org_id, uni_id)
-  
-  team_sch <- full_schedules %>%
-    filter(., team_id == start_team_id) %>%
-    select(., team_id, org_id, result)
-  
-  opp_sch <- NULL
-  opp_opp_sch <- NULL
-  
-  for(i in seq(1:nrow(list_opp))){
-    opp_sch <- rbind(opp_sch, full_schedules %>%
-                       filter(., team_id == list_opp[i,1],
-                              uni_id != list_opp[i,2]) %>%
-                       select(., team_id, org_id, result))
-  }
-  
-  for(i in seq(1:nrow(list_opp))){
-    
-    opp <- list_opp[i,1]
-    
-    opp_opp_list <- full_schedules %>%
-      filter(., team_id == opp) %>%
-      select(., org_id, uni_id)
-    
-    for(j in seq(1:nrow(opp_opp_list))){
-      
-      opp_opp_sch <- rbind(opp_opp_sch, full_schedules %>%
-                             filter(., team_id == opp_opp_list[j,1]) %>%
-                             select(., team_id, org_id, result))
-      
-    }
-    
-  }
-  
-  # .25(team_sch) + .5(opp_sch) + .25(opp_opp_sch)
-  
-  #0.25*mean(team_sch$result == "W", na.rm = TRUE) + 0.5*mean(opp_sch$result == "W", na.rm = TRUE) + 0.25*mean(opp_opp_sch$result == "W", na.rm = TRUE)
-  
-  
-  
-}
-
 calculate_rpi <- function(start_team_id){
   
   list_opp <- full_schedules %>%
@@ -143,11 +70,71 @@ calculate_rpi <- function(start_team_id){
   
   #mean(opp_opp_sch$result == "W", na.rm = TRUE)
   
+}
+
+recalculate_rpi <- function(start_team_id, game_to_replace, date_of_game_to_replace, new_game, des_result){
   
+  # get date object to replace
+  start_team_id <- as.integer(start_team_id)
+  game_to_replace <- as.character(game_to_replace)
+  date_of_game_to_replace_format <- mdy(date_of_game_to_replace)
   
+  replace_org <- filter(master_team_list, team == game_to_replace) %>%
+    pull(., team_id) %>%
+    as.integer(.)
   
+  list_opp <- full_schedules %>%
+    filter(., team_id == start_team_id,
+           !(opponent == game_to_replace & date == date_of_game_to_replace_format)) %>%
+    add_row(., team_id = as.integer(start_team_id),
+            org_id = replace_org,
+            date_str = date_of_game_to_replace,
+            date = date_of_game_to_replace_format,
+            opponent = game_to_replace,
+            location = "",
+            result = des_result,
+            score = "",
+            uni_id = "") %>%
+    select(., org_id, uni_id)
+  
+  team_sch <- full_schedules %>%
+    filter(., team_id == start_team_id) %>%
+    select(., team_id, org_id, result)
+  
+  opp_sch <- NULL
+  opp_opp_sch <- NULL
+  
+  for(i in seq(1:nrow(list_opp))){
+    opp_sch <- rbind(opp_sch, full_schedules %>%
+                       filter(., team_id == list_opp[i,1],
+                              uni_id != list_opp[i,2]) %>%
+                       select(., team_id, org_id, result))
+  }
+  
+  for(i in seq(1:nrow(list_opp))){
+    
+    opp <- list_opp[i,1]
+    
+    opp_opp_list <- full_schedules %>%
+      filter(., team_id == opp) %>%
+      select(., org_id, uni_id)
+    
+    for(j in seq(1:nrow(opp_opp_list))){
+      
+      opp_opp_sch <- rbind(opp_opp_sch, full_schedules %>%
+                             filter(., team_id == opp_opp_list[j,1]) %>%
+                             select(., team_id, org_id, result))
+      
+    }
+    
+  }
+  
+  # .25(team_sch) + .5(opp_sch) + .25(opp_opp_sch)
+  
+  0.25*mean(team_sch$result == "W", na.rm = TRUE) + 0.5*mean(opp_sch$result == "W", na.rm = TRUE) + 0.25*mean(opp_opp_sch$result == "W", na.rm = TRUE)
   
 }
+
 
 #-------------------------------------------------------------------------------
 # DEFINE SERVER LOGIC
@@ -224,10 +211,12 @@ server <- function(input, output) {
   output$rpi_unmod_calculated <- renderUI(
     {
       
-      team_id <- filter(master_team_list, team == input$start_team)$team_id
+      team_id <- filter(master_team_list, team == input$start_team) %>%
+        pull(., team_id) %>%
+        as.integer(.)
       
       game_to_replace <- input$game_to_replace
-      date_of_game_replaced <- input$selected_date_replace
+      date_of_game_replaced <- input$date_of_game_replaced
       selected_team_new <- input$selected_team_new
       result <- input$win_lose
       
@@ -235,8 +224,8 @@ server <- function(input, output) {
       rpi_after <- recalculate_rpi(team_id, game_to_replace, date_of_game_replaced, selected_team_new, result)
       
       HTML(
-        paste0("Before: ", round(rpi_before, 4), "<br/>", 
-               "After: ", "round(rpi_after, 4)", "<br/>")
+        paste0("Before: ", round(rpi_before, 8), "<br/>", 
+               "After: ", round(rpi_after, 8), "<br/>")
       )
     }
   )
--- scratch_code.R
+++ scratch_code.R
@@ -168,4 +168,122 @@ for(i in seq(1:nrow(list_opp))){
 
 
 
+##############################
+#recalculate_rpi <- function(start_team_id, game_to_replace, date_of_game_to_replace, new_game, des_result){
 
+
+date_of_game_to_replace <- "08/24/2018"
+date_of_game_to_replace_format <- mdy(date_of_game_to_replace)
+start_team_id <- 26172
+game_to_replace <- "South Carolina"
+new_game <- "Colorado St."
+des_result <- "W"
+
+replace_org <- filter(master_team_list, team == game_to_replace) %>%
+  pull(., team_id) %>%
+  as.integer(.)
+
+list_opp <- full_schedules %>%
+  filter(., team_id == start_team_id,
+         !(opponent == game_to_replace & date == date_of_game_to_replace_format)) %>%
+  add_row(., team_id = as.integer(start_team_id),
+          org_id = replace_org,
+          date_str = date_of_game_to_replace,
+          date = date_of_game_to_replace_format,
+          opponent = game_to_replace,
+          location = "",
+          result = des_result,
+          score = "",
+          uni_id = "") %>%
+  select(., org_id, uni_id)
+
+team_sch <- full_schedules %>%
+  filter(., team_id == start_team_id) %>%
+  select(., team_id, org_id, result)
+
+opp_sch <- NULL
+opp_opp_sch <- NULL
+
+for(i in seq(1:nrow(list_opp))){
+  opp_sch <- rbind(opp_sch, full_schedules %>%
+                     filter(., team_id == list_opp[i,1],
+                            uni_id != list_opp[i,2]) %>%
+                     select(., team_id, org_id, result))
+}
+
+for(i in seq(1:nrow(list_opp))){
+  
+  opp <- list_opp[i,1]
+  
+  opp_opp_list <- full_schedules %>%
+    filter(., team_id == opp) %>%
+    select(., org_id, uni_id)
+  
+  for(j in seq(1:nrow(opp_opp_list))){
+    
+    opp_opp_sch <- rbind(opp_opp_sch, full_schedules %>%
+                           filter(., team_id == opp_opp_list[j,1]) %>%
+                           select(., team_id, org_id, result))
+    
+  }
+  
+}
+
+# .25(team_sch) + .5(opp_sch) + .25(opp_opp_sch)
+
+0.25*mean(team_sch$result == "W", na.rm = TRUE) + 0.5*mean(opp_sch$result == "W", na.rm = TRUE) + 0.25*mean(opp_opp_sch$result == "W", na.rm = TRUE)
+
+recalculate_rpi(26172, "South Carolina", date_of_game_to_replace, "Colorado St.", "W")
+date_of_game_to_replace <- "08/24/2018"
+date_of_game_to_replace_format <- mdy(date_of_game_to_replace)
+start_team_id <- 26172
+game_to_replace <- "South Carolina"
+new_game <- "Colorado St."
+des_result <- "W"
+
+
+calculate_rpi <- function(start_team_id){
+  
+  list_opp <- full_schedules %>%
+    filter(., team_id == start_team_id) %>%
+    select(., org_id, uni_id)
+  
+  team_sch <- full_schedules %>%
+    filter(., team_id == start_team_id) %>%
+    select(., team_id, org_id, result)
+  
+  opp_sch <- NULL
+  opp_opp_sch <- NULL
+  
+  for(i in seq(1:nrow(list_opp))){
+    opp_sch <- rbind(opp_sch, full_schedules %>%
+                       filter(., team_id == list_opp[i,1],
+                              uni_id != list_opp[i,2]) %>%
+                       select(., team_id, org_id, result))
+  }
+  
+  for(i in seq(1:nrow(list_opp))){
+    
+    opp <- list_opp[i,1]
+    
+    opp_opp_list <- full_schedules %>%
+      filter(., team_id == opp) %>%
+      select(., org_id, uni_id)
+    
+    for(j in seq(1:nrow(opp_opp_list))){
+      
+      opp_opp_sch <- rbind(opp_opp_sch, full_schedules %>%
+                             filter(., team_id == opp_opp_list[j,1]) %>%
+                             select(., team_id, org_id, result))
+      
+    }
+    
+  }
+  
+  # .25(team_sch) + .5(opp_sch) + .25(opp_opp_sch)
+  
+  0.25*mean(team_sch$result == "W", na.rm = TRUE) + 0.5*mean(opp_sch$result == "W", na.rm = TRUE) + 0.25*mean(opp_opp_sch$result == "W", na.rm = TRUE)
+  
+  #mean(opp_opp_sch$result == "W", na.rm = TRUE)
+  
+}
